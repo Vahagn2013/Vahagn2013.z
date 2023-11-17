bcaf0e52886fcf98df391a58a48ca932caa1df66
Vahagn2013 <151218169+Vahagn2013@users.noreply.github.com>
2023-11-17 22:04:49 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// // xndir 1
+
+// let array = [800,`barev`,`500`,[1,2,3],`html`,79,null,NaN,55,`javascript`,{name:`Karen`}];
+
+
+// let a = array.map(function (val){
+//    if(typeof val === "number" && val === val){
+       
+//         return val*val
+
+//    }else{
+//     return "duq chuneq number"
+//    }
+// })
+// console.log(a);
+
+// xndir 2
+
+
+// let array = [800,`barev`,`500`,[1,2,3],`html`,79,null,NaN,55,`javascript`,{name:`Karen`}];
+
+
+// let a = array.map(function(val){
+//     if(typeof val === "string"){
+//         return val.toLocaleUpperCase()
+//     }else{
+//         return val
+//     }
+// })
+// console.log(a);
+
+// xndir 3
+
+// let array = [800,`barev`,`500`,[1,2,3],`html`,79,null,NaN,55,`javascript`,{name:`Karen`}];
+
+
+// let a = array.filter(function (val) {
+//   return typeof val === 'object' || Array.isArray(val);
+// });
+
+
+// console.log(a);
+
+// xndir 4
+
+// let array = [800,`barev`,`500`,[1,2,3],`html`,79,null,NaN,55,`javascript`,{name:`Karen`}];
+
+// let a = array.filter(function(val){
+//     if(typeof val === "number"){
+//         return val
+//     }
+    
+// })
+// console.log(a);
